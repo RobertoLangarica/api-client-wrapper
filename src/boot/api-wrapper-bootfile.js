13963d205c31567fc18462fb57979013681fe11a
Roberto <roberto@villavanilla.com>
2020-04-13 18:05:12 -0500

    - Deleting the bootfile since it is only needed in the quasar extension

--- src/boot/api-wrapper-bootfile.js
+++
@@ -1,8 +0,0 @@
-import api from '../wrapper/APIWrapper'
-import api_state from '../store/WrapperState'
-
-export default async ({ Vue, store }) => {
-  api.setStore(store)
-
-  Vue.prototype.$api = api;
-}
